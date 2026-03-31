261e27d7dd89e6238571f149c0de51727c5c1cb2
AlsornaJP <jpribeirojacob@gmail.com>
2026-03-31 04:19:04 -0300

    Exercícios 1 e 3 concluídos, exercício 2 incompleto

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercicio1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nome = "João Pedro";
+        DateOnly dataNascimento = new DateOnly(2001, 07, 05);
+        Console.WriteLine($"""
+                           Olá, meu nome é {nome}
+                           nasci em {dataNascimento.ToString("dd/MM/yyyy")} e estou aprendendo C#!
+                           """);
+    }
+}
