3ccd8ddcd570742eb2ef84802b0d9b031f7d4c6e
AlsornaJP <jpribeirojacob@gmail.com>
2026-04-01 00:11:48 -0300

    Exercício 10 concluído

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Exercicio10;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Console.WriteLine("Jogo de adivinhação");
+        Console.WriteLine("\nTente acertar o número secreto entre 1 e 50");
+
+
+        int tentativas = 5;
+        Random random = new Random();
+        int aleatorio = random.Next(1, 51);
+
+        while (tentativas > 0)
+        {
+            Console.WriteLine($"\nRestam {tentativas} tentativas");
+            try
+            {
+                if (!int.TryParse(Console.ReadLine(), out int escolha) || escolha > 50 || escolha < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(escolha), "Escolha um número válido");
+                }
+                if (escolha > aleatorio)
+                {
+                    Console.WriteLine($"O número secreto é MENOR que {escolha}"); 
+                    tentativas--;
+                }else if (escolha < aleatorio)
+                { 
+                    Console.WriteLine($"O número secreto é MAIOR que {escolha}"); tentativas--;
+                }
+                else { 
+                    Console.WriteLine("Parabéns, você acertou!"); 
+                    return;
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
